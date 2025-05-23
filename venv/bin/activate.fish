c61ac6814a3970930885ba928e1f173d7f25c9b7
donato <donato.junior@outlook.com.br>
2025-05-22 23:27:24 -0300

    Adicionado templates

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/junior/Documentos/BIBLIOTECA/venv
+set -gx VIRTUAL_ENV /home/donato/Documentos/PROJETOS/bibliotecaVirtual/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
